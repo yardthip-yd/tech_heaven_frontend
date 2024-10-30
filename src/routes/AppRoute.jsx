a5027cf24e9f3cdf70fbb1d12db032168d073018
yardthip-yd <yardthipp.d@gmail.com>
2024-10-30 13:21:40 +0700

    create-layout-page-route

---
+++ src/routes/AppRoute.jsx
@@ -0,0 +1,82 @@
+
+// Import
+const { Navigate, createBrowserRouter, RouterProvider } = require("react-router-dom");
+
+// Import Layouts
+import PageLayout from "@/layouts/PageLayout";
+
+
+// Import Pages
+const { default: Home } = require("@/pages/Home");
+const { default: Register } = require("@/pages/Register");
+
+
+// Import Store
+
+
+// Routing
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/register", element: <Register /> },
+        { path: "*", element: <Navigate to="/" /> },
+      ],
+    }])
+
+// Export AppRoute
+const AppRoute = () => {
+    return (
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    );
+  };
+  
+  export default AppRoute;
+
+
+// // Routing
+
+// const guestRouter = createBrowserRouter([
+//     {
+//         path: "/", element: ,
+//         children: [
+//             { index: true, element: <Home /> },
+//             { path: "/register", element: <Register /> },
+//             { path: "*", element: <Navigate to="/" /> },
+//         ],
+//     }
+// ]);
+
+// const userRouter = createBrowserRouter([
+//     {
+//         path: "/", element: <TripLayout />,
+//         children: [
+//             { index: true, element: <CreateTrip /> },
+//             { path: "/home", element: <Home /> },
+//             { path: "/user/account", element: <UserAccount /> },
+//             { path: "/admin/account", element: <AdminAccount /> },
+//             { path: "*", element: <Navigate to="/" /> },
+//         ]
+//     },
+// ])
+
+
+// // Export AppRoute
+
+// const AppRoute = () => {
+    
+//     // State for use authStore
+//     const user = useAuthStore((state) => state.user)
+
+//     const finalRouter = user ? userRouter : guestRouter
+
+//     return (
+//         <div>
+//             <RouterProvider router={finalRouter} />
+//         </div>
+//     );
+// };
