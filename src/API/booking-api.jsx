a8a3930474905c623fa978b8b42bad85e5168106
chonlathanme <chlmethanan@gmail.com>
2024-10-31 13:45:21 +0700

    Edit TextAreaAutoResize to Textarea

--- src/API/booking-api.jsx
+++ src/API/booking-api.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 export const createBooking = async (token, data) => {
-    try {
-        const response = await axios.post("http://localhost:8000/booking/create-booking", data, {
-            headers: {
-                headers: { Authorization: `Bearer ${"token"}` },
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error(error);
+  return await axios.post(
+    "http://localhost:8000/booking/create-booking",
+    data,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
+  );
+};
+
+export const getAllBookings = async (count = 20) => {
+  return await axios.get("http://localhost:8000/booking/get-all-bookings/"+count)
 }
