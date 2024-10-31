7aef321e9105725782f280f51874947447f887a1
Supakorn2539 <Supakorn.swa@gmail.com>
2024-10-31 12:33:44 +0700

    loggin google function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AppRoute from './routes/AppRoute'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 
 const App = () => {
   return (
     <div>
+      <GoogleOAuthProvider clientId='1083390196430-ikv6kj4bj4le7496hrvp9am2esq78sk4.apps.googleusercontent.com'>
       <AppRoute />
+      </GoogleOAuthProvider>
     </div>
   )
 }
