d0adfffc21077c4ef01d8f4c4401b459f12f0bd9
Supakorn2539 <Supakorn.swa@gmail.com>
2024-10-31 17:11:33 +0700

    google full

---
+++ src/components/home/PromotionSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PromotionSection = () => {
+  return (
+    <div className="h-screen w-full flex items-center max-h-[300px] bg-blue-400">PromotionSection</div>
+  )
+}
+
+export default PromotionSection
