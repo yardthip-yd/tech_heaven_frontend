66072d35f5c23f9b885c0ba634b501aec668a7eb
papinwit-l <pae.papinwit@gmail.com>
2024-11-08 10:51:05 +0700

    resolve conflict

--- src/components/home/AllProduct.jsx
+++ src/components/home/AllProduct.jsx
@@ -2,10 +2,7 @@ import React, { useEffect } from "react";
 import useProductStore from "@/stores/productStore";
 import ProductCard from "@/components/product/ProductCard";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import { ChevronRight } from "lucide-react";
->>>>>>> dev
 
 const AllProduct = () => {
   const { products, actionGetAllProducts, loading, error } = useProductStore();
@@ -30,16 +27,15 @@ const AllProduct = () => {
       productId: product.id,
       quantity: 1,
     };
-    console.log(itemToAdd, "item")
+    console.log(itemToAdd, "item");
     actionAddToCart(itemToAdd);
   };
-
 
   // Function to handle card click
   const handleCardClick = (productId) => {
     navigate(`/product/${productId}`);
-    console.log("product ID from all product", productId)
-  }
+    console.log("product ID from all product", productId);
+  };
   const handleViewAllClick = () => {
     navigate("/store");
   };
@@ -48,7 +44,10 @@ const AllProduct = () => {
     <div className="min-h-[420px] w-full px-8 max-w-[1440px] flex flex-col mx-auto pt-8">
       <div className="flex mb-4 flex-row gap-2 justify-between items-center">
         <p className="text-2xl font-bold">ALL PRODUCTS</p>
-        <div className="flex items-center cursor-pointer" onClick={handleViewAllClick}>
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={handleViewAllClick}
+        >
           <p className="text-blue-500 font-semibold">View All</p>
           <ChevronRight className="text-blue-500" />
         </div>
@@ -64,4 +63,3 @@ const AllProduct = () => {
 };
 
 export default AllProduct;
-
