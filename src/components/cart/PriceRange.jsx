dac463fb5236d97097edfc9847a4c4a669e2b325
yardthip-yd <yardthipp.d@gmail.com>
2024-11-11 16:17:12 +0700

    Revert "Revert "Merge branch 'feature/design' into dev""
    This reverts commit 29686a2adfe8e1b2e2abee4e27ef8ce71a0c8c00.

---
+++ src/components/cart/PriceRange.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import * as Slider from "@radix-ui/react-slider";
+
+const PriceRange = ({ priceRange, onPriceChange, min, max }) => {
+  const handleChange = (values) => {
+    onPriceChange(values);
+  };
+
+  return (
+    <div className="w-[250px]">
+      <Slider.Root
+        className="relative flex items-center select-none touch-none w-full h-5"
+        min={min}
+        max={max}
+        step={100}
+        value={priceRange}
+        onValueChange={handleChange}
+      >
+        <Slider.Track className="bg-slate-200 relative flex-grow rounded-full h-[6px]">
+          <Slider.Range className="absolute bg-blue-600 rounded-full h-full" />
+        </Slider.Track>
+        <Slider.Thumb className="block w-4 h-4 bg-blue-600 rounded-full shadow" />
+        <Slider.Thumb className="block w-4 h-4 bg-blue-600 rounded-full shadow" />
+      </Slider.Root>
+      <div className="flex items-center justify-between mt-4">
+        <div className="bg-slate-200 p-2 w-28 text-center rounded">{priceRange[0]}</div>
+        <span>-</span>
+        <div className="bg-slate-200 p-2 w-28 text-center rounded">{priceRange[1]}</div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceRange;
